874f2769a0f57dafef4a5026d686613677d87aa1
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-30 17:17:55 +0900

    refactor: Home コンポーネントの構造とスタイル

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>PageDesignGallery</h2>
-      <ol className={styles.lists}>
-        {links.map(({ href, label }) => (
-          <li key={href} className={styles.list}>
-            <Link href={href}>{label}</Link>
-          </li>
-        ))}
-      </ol>
+      <div className={styles.container}>
+        <h2 className={styles.title}>PageDesignGallery</h2>
+        <ol className={styles.lists}>
+          {links.map(({ href, label }) => (
+            <li key={href} className={styles.list}>
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
