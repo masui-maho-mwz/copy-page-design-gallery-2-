e7dd95a01cd5d0ce8793b5c74f50bf90150dac84
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-03 14:10:43 +0900

    delete: dashboard-menuのarticlesを削除

--- src/app/dashboard-menu/page.tsx
+++ src/app/dashboard-menu/page.tsx
@@ -1,4 +1,3 @@
-import { articles } from '@/app/dashboard-menu/articles-data';
 import { Articles } from '@/features/dashboard-menu/dashboard-and-main/main/articles';
 import { ContentsHeader } from '@/features/dashboard-menu/dashboard-and-main/main/contents-header';
 import styles from './styles.module.css';
@@ -8,7 +7,7 @@ export default function Home() {
     <div className={styles.root}>
       <div className={styles.contents}>
         <ContentsHeader />
-        <Articles articles={articles} />
+        <Articles />
       </div>
     </div>
   );
