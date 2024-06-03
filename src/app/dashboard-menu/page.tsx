3db36979e7b094a94f66542997a40ac8e35355cb
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-06-03 18:45:00 +0900

    delete: dashbord-menuの不要なコンポーネント（ContentsHeader）を削除

--- src/app/dashboard-menu/page.tsx
+++ src/app/dashboard-menu/page.tsx
@@ -1,12 +1,10 @@
 import { Articles } from '@/features/dashboard-menu/dashboard-and-main/main/articles';
-import { ContentsHeader } from '@/features/dashboard-menu/dashboard-and-main/main/contents-header';
 import styles from './styles.module.css';
 
 export default function Home() {
   return (
     <div className={styles.root}>
       <div className={styles.contents}>
-        <ContentsHeader />
         <Articles />
       </div>
     </div>
